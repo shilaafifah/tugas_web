e1705720431acb9ee2ec1d73be7cbddc0bc19dcc
Shila Afifah <shilaafifah@gmail.com>
2023-09-01 14:03:06 +0700

    Edit File operator1

--- operator1.php
+++ operator1.php
@@ -1,10 +1,16 @@
 <?php
-$tugas1=90;
-$tugas2=80;
-$jumlah=$tugas1 + $tugas2;
-$rerata=$jumlah/2 ;
-echo "Nilai Tugas I :".$tugas1."<br>";
-echo "Nilai Tugas II:".$tugas2."<br>";
-echo "Jumlah Tugas :".$jumlah."<br>";
-echo "Rerata Tugas :".$rerata;
+$tugas1 = 90;
+$tugas2 = 80;
+$jumlah = $tugas1 + $tugas2;
+$rerata = $jumlah / 2;
+
+// Menggabungkan nilai tugas1 dan tugas2 menjadi string 9080
+$gabungan = $tugas1 . $tugas2;
+
+echo "Nilai Tugas I : " . $tugas1 . "<br>";
+echo "Nilai Tugas II : " . $tugas2 . "<br>";
+echo "Jumlah Tugas : " . $jumlah . "<br>";
+echo "Rerata Tugas : " . $rerata . "<br>";
+
+echo "Gabungan Tugas : " . $gabungan;
 ?>
